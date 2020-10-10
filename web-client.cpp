7fc9c62f50130505e04a8bc1cf7fd5456d154153
crestanigabriel <gabrielpcrestani@gmail.com>
2020-10-10 11:02:09 -0300

    utilizando DNS para resolver o IP

--- web-client.cpp
+++ web-client.cpp
@@ -13,17 +13,65 @@
 #include "HTTPReq.cpp"
 #include "HTTPRes.cpp"
 
-using namespace std; 
+using namespace std;
+
+
+
+char* showIP(char *url) {
+  char* IP = new char[20];
+
+  struct addrinfo hints;
+  struct addrinfo* res;
+
+  //if (argc != 2) {
+  //  std::cerr << "usage: showip hostname" << std::endl;
+  // return 1;
+  //}
+
+  // hints - modo de configurar o socket para o tipo  de transporte
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET; // IPv4
+  hints.ai_socktype = SOCK_STREAM; // TCP
+
+  // funcao de obtencao do endereco via DNS - getaddrinfo
+  // funcao preenche o buffer "res" e obtem o codigo de resposta "status"
+  int status = 0;
+  if ((status = getaddrinfo(url, "80", &hints, &res)) != 0) {
+    std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+    strcpy(IP, "error");
+    return IP;
+  }
+
+  std::cout << "IP addresses for " << url << ": " << std::endl;
+
+  for(struct addrinfo* p = res; p != 0; p = p->ai_next) {
+    // a estrutura de dados eh generica e portanto precisa de type cast
+    struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+
+    // e depois eh preciso realizar a conversao do endereco IP para string
+    char ipstr[INET_ADDRSTRLEN] = {'\0'};
+    inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+    std::cout << "  " << ipstr << std::endl;
+
+    freeaddrinfo(res); // libera a memoria alocada dinamicamente para "res"
+    strcpy(IP, ipstr);
+    return IP;
+  }
+
+  freeaddrinfo(res); // libera a memoria alocada dinamicamente para "res"
+  strcpy(IP, "");
+  return IP;
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     cerr << "WRONG USAGE" << endl;
     cerr << "please, run './web-client [URL] [URL] ...'" << endl;
     return -1;
-  } 
+  }
 
-  // for (int i = 1; i < argc; ++i) 
-  //   cout << argv[i] << endl; 
+  // for (int i = 1; i < argc; ++i)
+  //   cout << argv[i] << endl;
   string url = argv[1];
 
   //! ------------------------------------------------
@@ -32,7 +80,7 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in serverAddr;
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(3000); //! estática??
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //! estática??
+  serverAddr.sin_addr.s_addr = inet_addr(showIP(argv[1])); //! estática??
   memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
 
   if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
@@ -58,7 +106,7 @@ int main(int argc, char *argv[]) {
   char buf[1024] = {0};
   memset(buf, '\0', sizeof(buf));
   request.buildMessage("GET", url);
-  
+
   if (send(sockfd, request.message.c_str(), request.message.size(), 0) == -1) {
     perror("send");
     return -1;
